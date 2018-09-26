165fc67c697d5e5ef1997d4f8b6048f26f519e41
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-26 09:38:27 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -96,7 +96,7 @@ describe("viewCart()", function() {
       `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, ${getCart()[1].itemName} at $${getCart()[1].itemPrice}, and ${getCart()[2].itemName} at $${getCart()[2].itemPrice}.`
     );
   });
-});
+//});
 
 // describe("total()", function() {
 //   it("adds up the price of all items in the cart", function() {
