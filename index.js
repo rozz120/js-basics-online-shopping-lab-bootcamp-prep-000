4ea3aa8e4f2a71ac6d495bd27952c0227fcf19aa
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-26 10:29:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,9 +83,7 @@ function placeOrder(cardNumber) {
   {
     totalCost = totalCost + getCart()[i].itemPrice;
   }
-  
   cart = [];
-  
   return("Your total cost is $" + totalCost + ", which will be charged to the card " + cardNumber + ".");
 }
 
