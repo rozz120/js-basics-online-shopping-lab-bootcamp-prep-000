05a4ce408d49a1d9f71cb05ff764dbfdfc1b4aa0
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-26 09:35:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,11 +27,11 @@ function viewCart ()
     return("In your cart, you have " + getCart()[0].itemName + " at $" + getCart()[0].itemPrice + ".")
   }
   
-  if(cart.length > 1)
+  if(cart.length === 2)
   {
+   "In your cart, you have " + getCart()[0].itemName + " at $" + getCart()[0].itemPrice + ", and "+ getCart()[1].itemName + " at $" + getCart()[1].itemPrice + ".") 
     
     
-    return("In your cart, you have " + )
   }
 }
 
