0ceae52c898eed2a6734a9504dd82abcb3f85655
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-26 09:04:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,42 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
-}
+function addToCart(item){
+  
+  var price = Math.floor((Math.random()*100)+1)
+  cart.push({itemName: item, itemPrice: price });
+  
+ 
+  
+  
+ 
+// cart.push(new Object({[item]:price}))
+// console.log(`${item} has been added to your cart.`)
+// return cart
+ }
 
-function viewCart() {
-  // write your code here
+function viewCart ()
+{
+
+}
+if(cart.length === 0){
+  console.log(`Your shopping cart is empty.`)
+}
+else{
+      var myString = "In your cart, you have "
+        for(var i = 0, l = cart.length; i < l; i++){
+            for(var list in cart[i]){
+            myString+=`${list} at $${cart[i][list]}`
+                if(i!==cart.length-1){
+                  myString+=", "
+                }
+                else{
+                  myString+="."
+                }
+              }
+            }
+      console.log(myString)
+  }
 }
 
 function total() {
